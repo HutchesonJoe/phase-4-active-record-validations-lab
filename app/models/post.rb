cee1eb02f241b0a6af08aec66944a4fb046739be
Joe <hutcheson2001@yahoo.com>
2022-05-16 14:50:13 -0400

    commit tree c612a954224640e52e01a45a1e2a0e4b067c3cf0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    titles = ["Won't Believe", "Secret", "Top #{/\d/}", "Guess"]
+    # byebug
+    unless title && titles.any? {|word| title.include? (word)}
+    errors.add(:title, "Not Click-baity enough!")
+    end
+  end
+
 end
